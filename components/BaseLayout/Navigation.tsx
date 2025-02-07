15acde73cda4950064e23cf19affce33112222e5
Andrew <xpynkuy3@gmail.com>
2025-02-07 17:20:24 +0500

    fix navigation comp

--- components/BaseLayout/Navigation.tsx
+++ components/BaseLayout/Navigation.tsx
@@ -1,18 +1,20 @@
+// components/Navigation.tsx
 import Link from "next/link";
-import React from "react";
-
 
 interface NavLink {
-    label: string;
-    href: string;
+  label: string;
+  href: string;
+  onClick?: () => void; 
 }
 
-const Navigation: React.FC<NavLink> = ({label, href}) => {
-    return (
-        <Link href={href}>
-            <div className="hover:text-white/70 transition ease-in-out active:text-gray-500">{label}</div>
-        </Link>
-    )
-}
+const Navigation: React.FC<NavLink> = ({ label, href, onClick }) => {
+  return (
+    <Link href={href} onClick={onClick}>
+      <div className="hover:text-white/70 transition ease-in-out active:text-gray-500">
+        {label}
+      </div>
+    </Link>
+  );
+};
 
-export {Navigation}
+export { Navigation };
